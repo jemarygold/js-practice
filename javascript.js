151561fe908e496110afbc7ddaf9573939591a14
jemarygold <jemarygold@gmail.com>
2024-01-21 19:29:03 +0000

    Complete JS excercise from TOP

--- javascript.js
+++ javascript.js
@@ -1,3 +1,39 @@
 // This will print to console in browser
 console.log("Hello World")
 
+// adding of two numbers 
+console.log("adding of two numbers")
+console.log(23 + 97) //should be 120
+
+// add multiple numbers
+console.log("add multiple numbers")
+console.log(1 + 2 + 3 + 4 + 5 + 6) //should 21
+ 
+
+// test an equation
+console.log("test an equation")
+console.log((4 + 6 + 9) / 77) //should be 0.24675
+
+// print the variable
+console.log("Print the variable")
+let a = 10; 
+console.log(a) // should be 10
+
+console.log("print the equation")
+console.log(9 * a) // should be 90
+
+
+console.log("print a second equation")
+let b = 7 * a
+console.log(b) // should be 70
+
+
+const MAX = 57;
+let actual = MAX - 13
+let percentage = actual / MAX
+
+console.log("Further equations")
+console.log(MAX)
+console.log(actual)
+console.log(percentage)
+
